5f70b51af8b843611382a2c7e3d08eaad17996d4
Tiffany McKenzie <tiffanymckenzie@Tiffanys-MacBook-Pro-2.local>
2019-12-02 21:47:01 -0800

    add trap cities in new world declaration

--- structs/world_struct.go
+++ structs/world_struct.go
@@ -14,27 +14,65 @@ type World struct {
 func NewWorld(worldName string, worldMap map[string][][]string) World {
 	world := World{Name: worldName}
 	nameToCity := make(map[string]*City)
+
+	//Create main cities listed
 	for cityName, neighborInfo := range worldMap {
 		pointerToCity, city := NewCity(cityName, neighborInfo)
 		world.Cities = append(world.Cities, city)
 		nameToCity[cityName] = pointerToCity
 	}
+
+	//Convert from direction from string to City type
 	for i := 0; i < len(world.Cities); i++ {
 		city := world.Cities[i]
 		if city.NorthByName != "" {
 			city.North = nameToCity[city.NorthByName]
+			if !contains(world.Cities, city.NorthByName) {
+				//northNeighbors := [][]string
+				northCityPointer, northCity := NewCity(city.NorthByName, nil)
+				world.Cities = append(world.Cities, northCity)
+				nameToCity[city.NorthByName] = northCityPointer
+			}
 		}
 		if city.SouthByName != "" {
 			city.South = nameToCity[city.SouthByName]
+			if !contains(world.Cities, city.SouthByName) {
+				//southNeighbors := [][]string
+				southCityPointer, southCity := NewCity(city.SouthByName, nil)
+				world.Cities = append(world.Cities, southCity)
+				nameToCity[city.SouthByName] = southCityPointer
+			}
 		}
 		if city.EastByName != "" {
 			city.East = nameToCity[city.EastByName]
+			if !contains(world.Cities, city.EastByName) {
+				//eastNeighbors := [][]string
+				eastCityPointer, eastCity := NewCity(city.EastByName, nil)
+				world.Cities = append(world.Cities, eastCity)
+				nameToCity[city.SouthByName] = eastCityPointer
+			}
 		}
 		if city.WestByName != "" {
 			city.West = nameToCity[city.WestByName]
+			if !contains(world.Cities, city.WestByName) {
+				//westNeighbors := [][]string
+				westCityPointer, westCity := NewCity(city.WestByName, nil)
+				world.Cities = append(world.Cities, westCity)
+				nameToCity[city.WestByName] = westCityPointer
+			}
 		}
 	}
+
 	return world
+}
+
+func contains(cities []City, cityName string) bool {
+	for i := range cities {
+		if cities[i].Name == cityName {
+			return true
+		}
+	}
+	return false
 }
 
 func (worldX World) LaunchInvasion(numAliens int) World {
@@ -83,6 +121,9 @@ func (worldX World) PrintMap() {
 			}
 			neighbors += direction + "=" + adjacentCity + " "
 		}
-		fmt.Println(city.Name, " ", neighbors)
+		if neighbors != "" {
+			fmt.Println(city.Name, " ", neighbors)
+		}
+
 	}
 }
